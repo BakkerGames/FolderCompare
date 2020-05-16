f30c38377be7e8eca4851d16ae5cd12c47f828f9
Scott Bakker <sbakker343@gmail.com>
2020-05-16 12:15:54 -0400

    Add project files.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace FolderCompare
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxMain = new System.Windows.Forms.ListBox();
+            this.textBoxFromPath = new System.Windows.Forms.TextBox();
+            this.textBoxToPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCompare = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxMain
+            // 
+            this.listBoxMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxMain.FormattingEnabled = true;
+            this.listBoxMain.Location = new System.Drawing.Point(12, 50);
+            this.listBoxMain.Name = "listBoxMain";
+            this.listBoxMain.Size = new System.Drawing.Size(776, 355);
+            this.listBoxMain.TabIndex = 0;
+            // 
+            // textBoxFromPath
+            // 
+            this.textBoxFromPath.Location = new System.Drawing.Point(12, 12);
+            this.textBoxFromPath.Name = "textBoxFromPath";
+            this.textBoxFromPath.Size = new System.Drawing.Size(361, 20);
+            this.textBoxFromPath.TabIndex = 1;
+            // 
+            // textBoxToPath
+            // 
+            this.textBoxToPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxToPath.Location = new System.Drawing.Point(427, 12);
+            this.textBoxToPath.Name = "textBoxToPath";
+            this.textBoxToPath.Size = new System.Drawing.Size(361, 20);
+            this.textBoxToPath.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(13, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonCompare
+            // 
+            this.buttonCompare.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonCompare.Location = new System.Drawing.Point(376, 415);
+            this.buttonCompare.Name = "buttonCompare";
+            this.buttonCompare.Size = new System.Drawing.Size(75, 23);
+            this.buttonCompare.TabIndex = 4;
+            this.buttonCompare.Text = "Compare";
+            this.buttonCompare.UseVisualStyleBackColor = true;
+            this.buttonCompare.Click += new System.EventHandler(this.buttonCompare_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(713, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonCompare);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxToPath);
+            this.Controls.Add(this.textBoxFromPath);
+            this.Controls.Add(this.listBoxMain);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Folder Compare";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxMain;
+        private System.Windows.Forms.TextBox textBoxFromPath;
+        private System.Windows.Forms.TextBox textBoxToPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonCompare;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,76 @@
+﻿using FolderCompare.Properties;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FolderCompare
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            textBoxFromPath.Text = Settings.Default.LastFromPath;
+            textBoxToPath.Text = Settings.Default.LastToPath;
+        }
+
+        private List<string> fromList = new List<string>();
+        private List<string> toList = new List<string>();
+
+        private void buttonCompare_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(textBoxFromPath.Text)) return;
+            if (!Directory.Exists(textBoxToPath.Text)) return;
+            Settings.Default.LastFromPath = textBoxFromPath.Text;
+            Settings.Default.LastToPath = textBoxToPath.Text;
+            Settings.Default.Save();
+            fromList.Clear();
+            toList.Clear();
+            listBoxMain.Items.Clear();
+            foreach (string s in Directory.EnumerateFiles(textBoxFromPath.Text))
+            {
+                if (ValidFilename(s))
+                {
+                    fromList.Add(s.Substring(s.LastIndexOf('\\') + 1));
+                }
+            }
+            foreach (string s in Directory.EnumerateFiles(textBoxToPath.Text))
+            {
+                if (ValidFilename(s))
+                {
+                    toList.Add(s.Substring(s.LastIndexOf('\\') + 1));
+                }
+            }
+            foreach (string s in fromList)
+            {
+                if (!toList.Contains(s))
+                {
+                    listBoxMain.Items.Add($"<-- {s}");
+                }
+                else
+                {
+                    FileInfo fileFrom = new FileInfo($"{textBoxFromPath.Text}\\{s}");
+                    FileInfo fileTo = new FileInfo($"{textBoxToPath.Text}\\{s}");
+                    if (fileFrom.Length != fileTo.Length)
+                    {
+                        listBoxMain.Items.Add($"<-> {s}");
+                    }
+                }
+            }
+            foreach (string s in toList)
+            {
+                if (!fromList.Contains(s))
+                {
+                    listBoxMain.Items.Add($"--> {s}");
+                }
+            }
+        }
+
+        private bool ValidFilename(string s)
+        {
+            if (s.EndsWith(".txt")) return true;
+            return false;
+        }
+    }
+}
